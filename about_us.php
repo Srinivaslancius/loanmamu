9f106b8e603e1b4215c88918df6d2a297c18bd3c
Srinivaslancius <srinivas@lanciussolutions.com>
2017-10-13 12:58:08 +0530

    new loanmamu htmls

---
+++ about_us.php
@@ -0,0 +1,110 @@
+<?php include_once "main_header.php"; ?>
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-breadcrumb">
+                        <ol class="breadcrumb">
+                            <li><a href="index.php">Home</a></li>
+                            <li class="active">About us</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class=" ">
+        <!-- content start -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="wrapper-content bg-white">
+                        <div class="about-section pinside40">
+                            <div class="row">
+                                <div class="col-md-offset-2 col-md-8 col-sm-12">
+                                    <div class="text-center section-space50">
+                                        <h1>About Us</h1>
+                                        <p style="text-align:justify">Our goal at Insight Loan Advisors is to provide access to personal loans, car loan, at insight competitive interest raa timely mannerlorem ipsums deconse resonescon.Our goal at Insight Loan Advisors is to provide access to personal loans, car loan, at insight competitive interest raa timely mannerlorem ipsums deconse resonescon. Our goal at Insight Loan Advisors is to provide access to personal loans, car loan, at insight competitive interest raa timely mannerlorem ipsums deconse resonescon. Our goal at Insight Loan Advisors is to provide access to personal loans, car loan, at insight competitive interest raa timely mannerlorem ipsums deconse resonescon. Our goal at Insight Loan Advisors is to provide access to personal loans, car loan, at insight competitive interest raa timely mannerlorem ipsums deconse resonescon.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="section-space80 bg-white">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8">
+										<div class="mb60 text-center section-title">
+											<h1>We are Here to Help You</h1>
+											<p>Our mission is to deliver reliable, latest news and opinions.</p>
+										</div>
+									</div>
+								</div>
+								<div class="row" style="padding-right:25px">
+									<div class="col-md-4 col-sm-6 col-xs-12">
+										<center>
+										<div class="bg-white bg-boxshadow pinside40 outline text-center mb30">
+											<div class="mb40"><i class="icon-calendar-3 icon-2x icon-default"></i></div>
+											<h2 class="capital-title">Apply For Loan</h2>
+											<p>Looking to buy a car or home loan? then apply for loan now.</p>
+											<a href="#" class="btn-link">Get Appointment</a>
+										</div>
+										</center>
+									</div>
+									<div class="col-md-4 col-sm-6 col-xs-12">
+										<center>
+										<div class="bg-white bg-boxshadow pinside40 outline text-center mb30">
+											<div class="mb40"><i class="icon-phone-call icon-2x icon-default"></i></div>
+											<h2 class="capital-title">Call us at </h2>
+											<h1 class="text-big">800-123-456 / 789 </h1>
+											<p>lnfo@loanadvisor.com</p>
+											<a href="#" class="btn-link">Contact us</a>
+										</div>
+										</center>
+									</div>
+									<div class="col-md-4 col-sm-6 col-xs-12">
+										<center>
+										<div class="bg-white bg-boxshadow pinside40 outline text-center mb30">
+											<div class="mb40"> <i class="icon-users icon-2x icon-default"></i></div>
+											<h2 class="capital-title">Talk to Advisor</h2>
+											<p>Need to loan advise? Talk to our Loan advisors.</p>
+											<a href="#" class="btn-link">Meet The Advisor</a>
+										</div>
+										</center>
+									</div>
+								</div>
+							</div>
+						</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.content end -->
+    <div class="footer section-space50">
+        <!-- footer -->
+        <?php include_once "footer.php"; ?>
+    </div>
+    <!-- /.footer -->
+    <div class="tiny-footer">
+        <!-- tiny footer -->
+         <?php include_once "footer_bottom.php"; ?>
+    </div>
+    <!-- /.tiny footer -->
+    <!-- back to top icon -->
+    <a href="#0" class="cd-top" title="Go to top">Top</a>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/menumaker.js"></script>
+    <!-- animsition -->
+    <script type="text/javascript" src="js/animsition.js"></script>
+    <script type="text/javascript" src="js/animsition-script.js"></script>
+    <!-- sticky header -->
+    <script type="text/javascript" src="js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="js/sticky-header.js"></script>
+    <!-- Back to top script -->
+    <script src="js/back-to-top.js" type="text/javascript"></script>
+<script src="js/preloader.js"></script>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,32 @@
+<div class="col-md-9 col-sm-12 col-xs-12">
+                    <div id="navigation">
+                        <!-- navigation start-->
+                        <ul>
+                            <li class="active"><a href="index.php">Home</a></li>
+							<li><a href="about_us.php">About us</a></li>       
+                            <li><a href="services.php">Services</a>
+                                <ul>
+                                    <li><a href="loan_loan.php">Home Loan</a></li>
+									<li><a href="loan_loan.php">Mortgage Loan</a></li>
+									<li><a href="loan_loan.php">Business Loan</a></li>
+									<li><a href="loan_loan.php">Personal Loan</a></li>
+									<li><a href="loan_loan.php">Car Loan Used Car Loan</a></li>
+									<li><a href="loan_loan.php">Life Insurence</a></li>
+									<li><a href="loan_loan.php">Car Insurence</a></li>
+									<li><a href="loan_loan.php">health Insurence</a></li>
+									<li><a href="loan_loan.php">Mutual Funds</a></li>
+                                </ul>
+                            </li>
+                                                
+                            <li><a href="#">Careers</a>
+                                <ul>
+								<li><a href="current_openings.php">Current Opening</a></li>
+								
+                                </ul>
+                            </li>
+							  <li><a href="faq.php">FAQ'S</a></li>
+                            <li><a href="contact_us.php">Contact us</a></li>
+                        </ul>
+                    </div>
+                    <!-- /.navigation start-->
+                </div>
