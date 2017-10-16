8a7288ce1b3d71f23c67a83a46d474680615f9f0
srinivaslancuius <srinivas@lanciussolutions.com>
2017-10-16 13:06:28 +0530

    changes in about_us file and current_openings file

--- about_us.php
+++ about_us.php
@@ -42,37 +42,7 @@
 									</div>
 								</div>
 								<div class="row" style="padding-right:25px">
-									<div class="col-md-4 col-sm-6 col-xs-12">
-										<center>
-										<div class="bg-white bg-boxshadow pinside40 outline text-center mb30">
-											<div class="mb40"><i class="icon-calendar-3 icon-2x icon-default"></i></div>
-											<h2 class="capital-title">Apply For Loan</h2>
-											<p>Looking to buy a car or home loan? then apply for loan now.</p>
-											<a href="#" class="btn-link">Get Appointment</a>
-										</div>
-										</center>
-									</div>
-									<div class="col-md-4 col-sm-6 col-xs-12">
-										<center>
-										<div class="bg-white bg-boxshadow pinside40 outline text-center mb30">
-											<div class="mb40"><i class="icon-phone-call icon-2x icon-default"></i></div>
-											<h2 class="capital-title">Call us at </h2>
-											<h1 class="text-big"><a href="Tel:<?php echo $getSiteSettingsData['mobile']; ?>"><?php echo $getSiteSettingsData['mobile'] ?></a> </h1>
-											<p><a href="mailto:<?php echo $getSiteSettingsData['email'];?>"><?php echo $getSiteSettingsData['email'];?></a></p>
-											<a href="#" class="btn-link">Contact us</a>
-										</div>
-										</center>
-									</div>
-									<div class="col-md-4 col-sm-6 col-xs-12">
-										<center>
-										<div class="bg-white bg-boxshadow pinside40 outline text-center mb30">
-											<div class="mb40"> <i class="icon-users icon-2x icon-default"></i></div>
-											<h2 class="capital-title">Talk to Advisor</h2>
-											<p>Need to loan advise? Talk to our Loan advisors.</p>
-											<a href="#" class="btn-link">Meet The Advisor</a>
-										</div>
-										</center>
-									</div>
+									<?php include_once "information.php"; ?>
 								</div>
 							</div>
 						</div>
