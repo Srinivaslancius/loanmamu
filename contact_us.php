b356acc2fe09947481c237fa0134294b7ac7148c
srinivaslancuius <srinivas@lanciussolutions.com>
2017-10-16 11:20:24 +0530

    changes in contact_us file

--- contact_us.php
+++ contact_us.php
@@ -105,8 +105,7 @@
                             </div>
                         </div>
                         <div class="map">
-						 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243647.316983494!2d78.26795745698291!3d17.41229980019287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99daeaebd2c7%3A0xae93b78392bafbc2!2sHyderabad%2C+Telangana!5e0!3m2!1sen!2sin!4v1507031550328" width="100%" height="420" frameborder="0" style="border:0" allowfullscreen></iframe> 
-                        </div>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243647.316983494!2d78.26795745698291!3d17.41229980019287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99daeaebd2c7%3A0xae93b78392bafbc2!2sHyderabad%2C+Telangana!5e0!3m2!1sen!2sin!4v1507031550328" width="100%" height="420" frameborder="0" style="border:0" allowfullscreen></iframe></div>
                     </div>
                 </div>
             </div>
