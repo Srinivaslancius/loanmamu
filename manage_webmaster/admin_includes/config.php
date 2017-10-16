e1287b50957f2277e455d5227b6960d00d1e1531
Srinivaslancius <srinivas@lanciussolutions.com>
2017-10-16 12:28:00 +0530

    new config remove conflict

--- manage_webmaster/admin_includes/config.php
+++ manage_webmaster/admin_includes/config.php
@@ -10,7 +10,7 @@ if($setcon == 1) {
 	$password = "";
 	$dbname = "";
 } else {
-	$servername = "192.168.0.105";	
+	$servername = "192.168.0.114";	
 	$username = "root";
 	$password = "root";
 	$dbname = "loanmamu";
@@ -21,8 +21,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}  
 
-$base_url = "http://192.168.0.105/loanmamu/";
+$base_url = "http://192.168.0.114/loanmamu/";
 
 ?>
