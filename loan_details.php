d409be5ee7a4390526f0a9c510d9bb8f717f687b
Srinivaslancius <srinivas@lanciussolutions.com>
2017-10-17 16:18:13 +0530

    loan details changes

--- loan_details.php
+++ loan_details.php
@@ -33,7 +33,7 @@ $getAllServiceData = $conn->query($sqlMultiple);
                         </div>
                         <div class="compare-block mb30 prdct">
 							<div class="compare-row outline pinside30 padd0">
-								<?php while($getAllProducts = $getAllServiceData->fetch_assoc()) { ?>
+								<?php $i=1; while($getAllProducts = $getAllServiceData->fetch_assoc()) { ?>
 								<div class="row brdrbtm">
                                     <div class="col-md-2 col-sm-12 col-xs-12 paddrl0">
 										<div class="rate-counter-block-one">
@@ -87,7 +87,7 @@ $getAllServiceData = $conn->query($sqlMultiple);
 											<div class="rate-box">
 											<center>
 												<div class="btn-group">
-													<a href="javascript:void(0);" id="btn_loan" onClick="showDetails(1);" class="btn btn-default btn-sm btn_loan">Details</a>
+													<a href="javascript:void(0);" id="btn_loan" onClick="showDetails(<?php echo $i; ?>);" class="btn btn-default btn-sm btn_loan">Details</a>
 													<a href="#" class="btn btn-default btn-sm">Submit</a>
 												</div>
 											</center>
@@ -95,82 +95,14 @@ $getAllServiceData = $conn->query($sqlMultiple);
 									  </div>
                                     </div>
                                 </div>
-                                <?php } ?>
 
-								
-								
-								<div class="row brdrbtm" id="details-block-1" style="display: none; padding: 20px;">
-									<div class="col-md-offset-2 col-md-8 col-sm-12">
-										<div class="section-space50">
-											<h4>What you'll love</h4>
-												<ul>
-													<li>No guarantor required</li>
-													<li>No pre-closure and part payment fee</li>
-												</ul>
-										</div>
-									</div>
-									<div class="col-md-offset-2 col-md-8 col-sm-12">
-										<div class="section-space50">
-											<h4>Documents</h4>
-											<ul>
-												<li>KYC-PAN, address & ID proof</li>
-												<li>Income proof, bank statement and one photograph</li>
-											</ul>
-										</div>
-									</div>
-									<div class="col-md-offset-2 col-md-8 col-sm-12">
-										<div class="section-space50">
-											<h4>Perks</h4>
-											<ul>
-												<div class="row">
-													<div class="col-md-6 col-sm-6 col-xs-12">
-														<li>Door step service</li>
-														<li>Part-payment available</li>
-														<li>No guarantor required</li>
-													</div>
-													<div class="col-md-6 col-sm-6 col-xs-12">
-														<li>E-approval</li>
-														<li>Top-up loan available</li>
-														<li>Balance transfer</li>
-													</div>
-												</div>
-											</ul>
-										</div>
-									</div>
-									<div class="col-md-offset-2 col-md-8 col-sm-12">
-										<div class="section-space50">
-											<div class="row">
-												<div class="col-md-6 col-sm-6 col-xs-12">
-													<h4>Fees & Charges</h4>
-													<ul>
-														<li>Preclosure fee = ₹ 0 <span class= "glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="But preclosur charges of 2% are levied in cause of shifting of housing loan A/C by takeover to other banks/ housing finance institutions"></span></li>
-														<li>Part payment fee = ₹ 0 <span class= "glyphicon glyphicon-question-sign" data-toggle="tooltip" data-placement="right" title="But preclosur charges of 2% are levied in cause of shifting of housing loan A/C by takeover to other banks/ housing finance institutions"></span></li>
-													</ul>
-												</div>
-												<div class="col-md-6 col-sm-6 col-xs-12">
-													<h4>Good to Know</h4>
-													<p>Branches<br>38</p>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="col-md-offset-2 col-md-8 col-sm-12">
-										<div class="section-space50">
-											<h4>Eligibility Criteria</h4>
-											<ul>
-												<li>Age at least 23 and less than 70</li>
-											</ul>
-										</div>
-									</div>
+                                <div class="row brdrbtm" id="details-block-<?php echo $i; ?>" style="display: none; padding: 20px;">
+									<?php echo $getAllProducts['description']; ?>
 								</div>
+                                <?php $i++; } ?>
+								
                             </div>
 						</div>	
-
-
-
-
-
-					
 
 
                     </div>
@@ -213,16 +145,8 @@ $getAllServiceData = $conn->query($sqlMultiple);
 	
 
 	<script>
-		var showDetails = function(value){			
-				if(value == 1){
-					$('#details-block-1').toggle().addClass('animated fadeInUp');
-				}else if(value == 2){
-					$('#details-block-2').toggle().addClass('animated fadeInUp');
-				}else if(value == 3){
-					$('#details-block-3').toggle().addClass('animated fadeInUp');
-				}else if(value == 4){
-					$('#details-block-4').toggle().addClass('animated fadeInUp');
-				}							
+		var showDetails = function(value){
+				$('#details-block-'+value).toggle().addClass('animated fadeInUp');	
 			};		
 	</script>
 </body>
