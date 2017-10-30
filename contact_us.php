ca2e183f3f9ba262ebdcc7651db7926ce5a0c3cd
srinivaslancuius <srinivas@lanciussolutions.com>
2017-10-30 16:25:43 +0530

    changes in contact_us file

--- contact_us.php
+++ contact_us.php
@@ -1,33 +1,38 @@
 <?php include_once "main_header.php"; ?>
 <?php
+//ob_start();
 if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['mobile']) && !empty($_POST['message']))  {
+
 $dataem = $getSiteSettingsData['email'];
 //$to = "srinivas@lanciussolutions.com";
 $to = "$dataem";
-$subject = 'Contact Request Submitted by '.$name;
+$subject = "Contact Request Submitted by ".$name;
 
-$message = "<html><head><title><h2>Contact Request Submitted</h2> </title></head>
+$message = "<html><head><title>Loanmamu </title></head>
 <body>
+<p>Contact Request Submitted!</p>
 <h4>Name: </h4><p>".$_POST['name']."</p>
 <h4>Email: </h4><p>".$_POST['email']."</p>
-<h4>Mobile: </h4><p>".$_POST['mobile']."</p>
+<h4>Email: </h4><p>".$_POST['mobile']."</p>
 <h4>Message: </h4><p>".$_POST['message']."</p>
 </body>
 </html>
 ";
+
 // Always set content-type when sending HTML email
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
 $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
-
+// $headers .= 'Cc: myboss@example.com' . "\r\n";
 
 if(mail($to,$subject,$message,$headers)) {
-    echo  "<script>alert('Message Sent successfully.');window.location.href('contact_us.php');</script>";
+    echo  "<script>alert('Mail Sent successfully');window.location.href('contact_us.php');</script>";
 }
 
 }?>
+<!DOCTYPE html>
     <div class="page-header">
         <div class="container">
             <div class="row">
