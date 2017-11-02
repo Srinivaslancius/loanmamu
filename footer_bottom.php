a6f885b2c90f0db706e66ce6658d0ea6a432fc0e
srinivaslancuius <srinivas@lanciussolutions.com>
2017-11-02 13:13:39 +0530

    creates terms and policy files

--- footer_bottom.php
+++ footer_bottom.php
@@ -4,7 +4,7 @@
                     <p>© Copyright 2017 | Designed By <a target="_blank" href="https://www.lanciussolutions.com/"> Lancius Solutions</a></p>
                 </div>
                 <div class="col-md-6 col-sm-6 text-right col-xs-6">
-                    <p>Terms of use | Privacy Policy</p>
+                    <p><a href="terms_of_policy.php">Terms of use | Privacy Policy</a></p>
                 </div>
             </div>
         </div>
